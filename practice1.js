33455df665791b38a78e90d119207290517cd400
王嘉巍 <906789088@qq.com>
2019-06-28 00:03:08 +0800

    push don't equal zero data push newArray to change finish filter

--- practice1.js
+++ practice1.js
@@ -6,3 +6,13 @@ for(var i = 1;i < a.length; i++) {
   c += b + a[i];
 }
 console.log(c)
+
+//编写一段数组将不为0的数字移动到一个新的数组当中
+var arr = [0, 21, 34, 0, 100, 65, 101];
+var newArr = [];
+for(var i = 0;i < arr.length; i++) {
+  if (arr[i] != 0) {
+    newArr[newArr.length] = arr[i];
+  }
+}
+console.log(newArr)
