395411a0cfcd1023ed7949cfb2589435b33329bc
王嘉巍 <906789088@qq.com>
2019-07-22 12:07:32 +0800

    change rgb color math.random and create to this data

--- js-demo/practice8.js
+++ js-demo/practice8.js
@@ -3,3 +3,15 @@ function getRandom(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min)
 }
 var c = getRandom(0, 100);
+
+// 随机产生rgb颜色
+function getRandom(min ,max) {
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+function getRGB(min, max) {
+  var col1 = getRandom(min, max);
+  var col2 = getRandom(min, max);
+  var col3 = getRandom(min, max);
+  return 'rgb('+ col1 + ',' + col2 + ',' + col3 + ')'
+}
+var d = getRGB(0, 255)
