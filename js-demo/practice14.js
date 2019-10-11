b95049979ee822f4bde38a5377c294260d7a9199
王嘉巍 <906789088@qq.com>
2019-10-11 10:51:43 +0800

    practice useEffect and useState in these hooks

--- js-demo/practice14.js
+++ js-demo/practice14.js
@@ -1,12 +1,26 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 
-const practice = () => {
-  const [ count, setCount ] = useState(0);
+// const practice = () => {
+//   const [ count, setCount ] = useState(0);
+//   return (
+//     <div>
+//       <p>You clicked { count } times</p>
+//       <button onClick={() => {setCount( count + 1)}}>Click me</button>
+//     </div>
+//   )
+// }
+// export default practice;
+import React, { useState, useEffect } from 'react'
+const PracticeUseEffect = () => {
+  const [ count, setCount] = useState[0]
+  useEffect(() => {
+    document.title = `you clicked ${ count } times`
+  })
   return (
     <div>
       <p>You clicked { count } times</p>
-      <button onClick={() => {setCount( count + 1)}}>Click me</button>
+      <button onClick = {setCount(count + 1)}>Clicked me</button>
     </div>
   )
 }
-export default practice;
+export default PracticeUseEffect
