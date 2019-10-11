bfab79d5994ee2f5f3e6cb7fd3bba2033e06f1cb
王嘉巍 <906789088@qq.com>
2019-10-11 15:30:07 +0800

    another effect in submit friends online set in useEffect

--- js-demo/practice14.js
+++ js-demo/practice14.js
@@ -1,16 +1,19 @@
-// import React, { useState } from 'react';
+// useState
+import React, { useState } from 'react';
 
-// const practice = () => {
-//   const [ count, setCount ] = useState(0);
-//   return (
-//     <div>
-//       <p>You clicked { count } times</p>
-//       <button onClick={() => {setCount( count + 1)}}>Click me</button>
-//     </div>
-//   )
-// }
-// export default practice;
+const practice = () => {
+  const [ count, setCount ] = useState(0);
+  return (
+    <div>
+      <p>You clicked { count } times</p>
+      <button onClick={() => {setCount( count + 1)}}>Click me</button>
+    </div>
+  )
+}
+export default practice;
+//useEffect
 import React, { useState, useEffect } from 'react'
+
 const PracticeUseEffect = () => {
   const [ count, setCount] = useState[0]
   useEffect(() => {
@@ -24,3 +27,22 @@ const PracticeUseEffect = () => {
   )
 }
 export default PracticeUseEffect
+//副作用订阅好友状态在线
+import React, { useState, useEffect } from 'react'
+const FriendStatus = (props) => {
+  const [ isOnline, setIsOnline] = useState(null)
+  function handleStatusChange(status) {
+    setIsOnline(status.isOnline)
+  }
+  useEffect(() => {
+    ChatAPI.subscribeToFriendStatus(props.friend.id, handleStatusChange)
+    return () => {
+      ChatAPI.unsubscribeFromFriendStatus(props.friend.id, handleStatusChange)
+    }
+  })
+  if (isOnline === null) {
+    return 'loading...'
+  }
+  return isOnline ? 'Online' : 'Offline'
+}
+export default FriendStatus
