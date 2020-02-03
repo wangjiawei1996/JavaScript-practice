57049a4f23312a78f66bbe4e2e54ada5a5c54489
王嘉巍 <906789088@qq.com>
2020-02-03 16:55:43 +0800

    realize loader to find ! js file in webpack

--- js-demo/practice15.js
+++ js-demo/practice15.js
@@ -2,6 +2,12 @@ const path = require('path')
 module.exports = {
   mode: 'development',
   entry: './index.js',
+  module: {
+    rules: [{
+      test: /\.jpg$/,
+      loader: 'file-loader'
+    }]
+  },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
