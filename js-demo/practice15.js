e1c1eadb77aac5c5708e83e0a1d2c2dbd9c7285b
王嘉巍 <906789088@qq.com>
2020-02-09 17:58:40 +0800

    CleanWebpackPlugin,HtmlWebpackPlugin use in webpack

--- js-demo/practice15.js
+++ js-demo/practice15.js
@@ -1,4 +1,6 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 module.exports = {
   mode: 'development',
   entry: './index.js',
@@ -16,6 +18,9 @@ module.exports = {
       loader: ['style-loader', 'css-loader']
     }]
   },
+  plugins: [new HtmlWebpackPlugin({
+    template: 'src/index.html'
+  }),new CleanWebpackPlugin('dist')],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
